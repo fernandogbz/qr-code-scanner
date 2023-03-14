0fa4e987d072cb331e1b5e0de6cb75cebf01b10d
Fernando Jimenez <fgjv06@gmail.com>
2023-03-14 20:13:18 -0300

    JS - Getting user selected file

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ const wrapper = document.querySelector(".wrapper");
 form = wrapper.querySelector("form");
 fileInp = form.querySelector("input");
 
+fileInp.addEventListener("change", e => {
+  // Getting user selected file
+  let file = e.target.files[0];
+  console.log(file);
+});
+
 form.addEventListener("click", () => fileInp.click());

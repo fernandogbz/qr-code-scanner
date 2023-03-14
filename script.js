150960faa34b239eb65d1306985478705c5d7d26
Fernando Jimenez <fgjv06@gmail.com>
2023-03-14 20:29:12 -0300

    JS - Sending post request to qr server api with passing

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ const wrapper = document.querySelector(".wrapper");
 form = wrapper.querySelector("form");
 fileInp = form.querySelector("input");
 
+function fetchRequest(formData) {
+  fetch("http://api.qrserver.com/v1/read-qr-code/", {
+    // Sending post request to qr server api with passing
+    method:"POST", body: formData
+  })
+}
+
 fileInp.addEventListener("change", e => {
   let file = e.target.files[0]; // Getting user selected file
   let formData = new FormData(); // Creating a new FormData object
   formData.append("file", file); // Adding selected file to formData
-  console.log(file);
+  fetchRequest(formData);
 });
 
 form.addEventListener("click", () => fileInp.click());

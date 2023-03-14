849d0e38ca83e22c61409094884be6f064fd988b
Fernando Jimenez <fgjv06@gmail.com>
2023-03-14 20:16:52 -0300

    JS - Adding selected file to formData

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ fileInp = form.querySelector("input");
 fileInp.addEventListener("change", e => {
   let file = e.target.files[0]; // Getting user selected file
   let formData = new FormData(); // Creating a new FormData object
+  formData.append("file", file); // Adding selected file to formData
   console.log(file);
 });
 

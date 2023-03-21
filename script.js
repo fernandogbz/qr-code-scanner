1b2a699d504c2af50ce7c5aa2563a17ae5af1493
Fernando Jimenez <fgjv06@gmail.com>
2023-03-21 06:42:32 -0300

    JS - 'Scanning QR Code...' text while scannig qr code added

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const wrapper = document.querySelector(".wrapper");
 form = wrapper.querySelector("form");
 fileInp = form.querySelector("input");
+infoText = form.querySelector("p");
 
 function fetchRequest(formData) {
+  infoText.innerText = "Scanning QR Code...";
   // Sending post request to qr server api with passing
   // form data as body and getting response from it
   fetch("http://api.qrserver.com/v1/read-qr-code/", {
     method:"POST", body: formData
   }).then(res => res.json()).then(result => {
+    infoText.innerText = "Upload QR Code to Scan";
     wrapper.classList.add("active");
   console.log(result); 
   }); 
